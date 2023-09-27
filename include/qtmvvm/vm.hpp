52b437da6e73f23587840c5644459a6fd65f9393
decamel <den_spb97@mail.ru>
2023-09-27 17:01:19 +0300

    model based and vm iface separation

--- include/qtmvvm/vm.hpp
+++ include/qtmvvm/vm.hpp
@@ -16,8 +16,8 @@
 /* -------------------------------------------------------------------------- */
 namespace qtmvvm
 {
-  template <class IView, class Model>
-  class ViewModel : public IViewModel<IView>
+  template <class Model>
+  class ViewModel
   {
   public:
     using ModelPtr = std::shared_ptr<Model>;
@@ -26,26 +26,6 @@ namespace qtmvvm
 
   public:
     ViewModel(ModelPtr a_model) : model_(a_model) {}
-
-  protected:
-    /**
-     * @brief Connects necessary signals and slots of <IView> and this <WidgetModel>
-     *
-     * @param view Connectable view
-     */
-    virtual void connectView() const {};
-
-    /**
-     * @brief Refreshes view connected with this <WidgetModel>
-     *
-     * Must call super
-     *
-     * @param view <IView> which should be refreshed
-     */
-    virtual void refreshView()
-    {
-      QCoreApplication::processEvents();
-    };
   };
 } // namespace qtmvvm
 
